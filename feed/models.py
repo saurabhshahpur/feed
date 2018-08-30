19541a74ea10588e0f15ad9b043488c99e01c3be
Saurabh Singh <saurabhshahpur@gmail.com>
2018-08-30 14:03:44 +0530

    temp

--- feed/models.py
+++ feed/models.py
@@ -2,6 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser, models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
-
+class User(AbstractUser):
+    pass
