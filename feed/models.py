0b7ee1d995d827d47922490da6869b7216158459
Saurabh Singh <saurabhshahpur@gmail.com>
2018-08-31 14:44:04 +0530

    temp

--- feed/models.py
+++ feed/models.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class User(AbstractUser):
-    pass
+class UserDetail(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "user_detail"
--- users/views.py
+++ users/views.py
@@ -1,14 +1,17 @@
 import logging
 
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from rest_framework import viewsets, status
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.utils import json
 
-# from feed.models import User
+from feed.models import UserDetail
+from feed.serializers import UserSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -31,8 +34,10 @@ class UserViewSet(viewsets.ViewSet):
     """
 
     @authentication_classes((TokenAuthentication,))
-    @permission_classes((IsAuthenticated,))
+    @permission_classes((IsAuthenticated, IsAdminUser))
     def list(self, request):
+        queryset = UserDetail.objects.all()
+        serializer_class = UserSerializer
         return Response({"Success": True, "msg": "Ok"})
 
     """
@@ -42,8 +47,11 @@ class UserViewSet(viewsets.ViewSet):
     def create(self, request):
         user_name = request.data["user_name"]
         password = request.data["password"]
-        # User.objects.create(username=user_name, password=password, email=user_name, is_staff=True)
-        return Response({"success": "True", "msg": "user created"})
+        auth_user = User.objects.create(username=user_name, password=password, email=user_name, is_staff=True)
+        UserDetail.objects.create(user=auth_user)
+        token = Token.objects.get_or_create(user=auth_user)
+        token_data = str(token[0].key)
+        return Response({"success": "True", "msg": "user created", "token":token_data})
 
     def retrieve(self, request, pk=None):
         return Response({"Success": True, "msg": "Ok"})
